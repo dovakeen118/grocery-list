65de9b7a4aea4ac0c922625f4191e95dcd980846
dovakeen118 <kegillis118@gmail.com>
2019-07-30 11:27:42 -0400

    removed categories - not currently using

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,25 +1,7 @@
 class List < ApplicationRecord
 
-  CATEGORIES = [
-    "Fruit",
-    "Vegetables",
-    "Dairy",
-    "Red Meat",
-    "White Meat/ Poultry",
-    "Seafood",
-    "Grains",
-    "Canned Foods",
-    "Snacks",
-    "Beverages",
-    "Frozen",
-    "Seasoning/ Spices",
-    "Miscellaneous"
-  ]
-
   belongs_to :user
 
   validates :list_name, presence: true
-  validates :category,
-    presence: true,
-    inclusion: { in: CATEGORIES }
+
 end
