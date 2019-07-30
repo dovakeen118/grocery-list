f7dab6ab7c213d23fd94dcaa711e20f77e2f0461
dovakeen118 <kegillis118@gmail.com>
2019-07-30 11:18:31 -0400

    index page to React, routes updated

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,25 @@
 class List < ApplicationRecord
+
+  CATEGORIES = [
+    "Fruit",
+    "Vegetables",
+    "Dairy",
+    "Red Meat",
+    "White Meat/ Poultry",
+    "Seafood",
+    "Grains",
+    "Canned Foods",
+    "Snacks",
+    "Beverages",
+    "Frozen",
+    "Seasoning/ Spices",
+    "Miscellaneous"
+  ]
+
   belongs_to :user
 
   validates :list_name, presence: true
+  validates :category,
+    presence: true,
+    inclusion: { in: CATEGORIES }
 end
